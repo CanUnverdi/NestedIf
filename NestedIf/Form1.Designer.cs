b269023860fb03157b88e1443ba8ef7ac5493186
Can Ünverdi <53235845+CanUnverdi@users.noreply.github.com>
2019-08-02 23:00:51 +0300

    Nested

--- NestedIf/Form1.Designer.cs
+++ NestedIf/Form1.Designer.cs
@@ -28,13 +28,133 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAdSoyad = new System.Windows.Forms.TextBox();
+            this.txtYas = new System.Windows.Forms.TextBox();
+            this.cmbDurum = new System.Windows.Forms.ComboBox();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.lblUcret = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 54);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ad Soyad:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(56, 109);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 24);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Yaş:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(56, 168);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 24);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Durumu:";
+            // 
+            // txtAdSoyad
+            // 
+            this.txtAdSoyad.Location = new System.Drawing.Point(180, 48);
+            this.txtAdSoyad.Margin = new System.Windows.Forms.Padding(6);
+            this.txtAdSoyad.Name = "txtAdSoyad";
+            this.txtAdSoyad.Size = new System.Drawing.Size(238, 29);
+            this.txtAdSoyad.TabIndex = 1;
+            this.txtAdSoyad.TextChanged += new System.EventHandler(this.txtAdSoyad_TextChanged);
+            // 
+            // txtYas
+            // 
+            this.txtYas.Location = new System.Drawing.Point(184, 106);
+            this.txtYas.Margin = new System.Windows.Forms.Padding(6);
+            this.txtYas.Name = "txtYas";
+            this.txtYas.Size = new System.Drawing.Size(234, 29);
+            this.txtYas.TabIndex = 1;
+            // 
+            // cmbDurum
+            // 
+            this.cmbDurum.FormattingEnabled = true;
+            this.cmbDurum.Items.AddRange(new object[] {
+            "Normal",
+            "Öğrenci - 50",
+            "Öğretmen -30",
+            "Asker-50",
+            "Yaşlı-25",
+            "Engelli-75"});
+            this.cmbDurum.Location = new System.Drawing.Point(180, 162);
+            this.cmbDurum.Margin = new System.Windows.Forms.Padding(6);
+            this.cmbDurum.Name = "cmbDurum";
+            this.cmbDurum.Size = new System.Drawing.Size(238, 32);
+            this.cmbDurum.TabIndex = 2;
+            this.cmbDurum.SelectedIndexChanged += new System.EventHandler(this.cmbDurum_SelectedIndexChanged);
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.Location = new System.Drawing.Point(268, 216);
+            this.btnHesapla.Margin = new System.Windows.Forms.Padding(6);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(150, 42);
+            this.btnHesapla.TabIndex = 3;
+            this.btnHesapla.Text = "Ücret Hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = true;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // lblUcret
+            // 
+            this.lblUcret.AutoSize = true;
+            this.lblUcret.Location = new System.Drawing.Point(362, 285);
+            this.lblUcret.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblUcret.Name = "lblUcret";
+            this.lblUcret.Size = new System.Drawing.Size(56, 24);
+            this.lblUcret.TabIndex = 0;
+            this.lblUcret.Text = "__TL";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(542, 337);
+            this.Controls.Add(this.btnHesapla);
+            this.Controls.Add(this.cmbDurum);
+            this.Controls.Add(this.txtYas);
+            this.Controls.Add(this.txtAdSoyad);
+            this.Controls.Add(this.lblUcret);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtAdSoyad;
+        private System.Windows.Forms.TextBox txtYas;
+        private System.Windows.Forms.ComboBox cmbDurum;
+        private System.Windows.Forms.Button btnHesapla;
+        private System.Windows.Forms.Label lblUcret;
     }
 }
 
--- NestedIf/Form1.cs
+++ NestedIf/Form1.cs
@@ -16,5 +16,95 @@ namespace NestedIf
         {
             InitializeComponent();
         }
+
+        /*Şartlar:
+         1.Normal: İndirim Yok - 5Tl
+         2.Öğrenci: %50 indirim- Yaşı 30'dan büyükse indirim yok. 2.5TL
+         3.Öğretmen: %30 indirim- Yaşı 20'den küçükse indirim yok. 3.5TL
+         4.Asker: %50 indiirm - Yaşı 20'den küçğkse ve 65'den büüykse indirm yok. 2.5 TL
+         5.ENgelli: %75 indirm - 1.25TL
+         6.Yaşlı: %25 indirm yaşı 60'tan küçükse indirim yok 3.75 tl
+         /Ücret hesaplayınız.Eğer seçim yapmamışsa hata mesajı gösteriniz
+             */
+
+        //cmbDurum.SelectedIndex == -1 gelirse hiçbir değer seçilmmiş demektir
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            int yas = int.Parse(txtYas.Text);
+
+            if (cmbDurum.SelectedIndex ==-1)
+            {
+                lblUcret.Text = "Seçim Yapmadınız!";
+            }
+            else if (cmbDurum.SelectedIndex==0)
+            {
+                lblUcret.Text = "5TL";
+            }
+            else if (cmbDurum.SelectedIndex==1)
+            {
+                if (yas <30)
+                {
+                    lblUcret.Text = "2.5TL";
+                }
+                else
+                {
+                    lblUcret.Text = "5TL";
+                }
+            }
+            else if (cmbDurum.SelectedIndex==2)
+            {
+                if (yas <20)
+                {
+                    lblUcret.Text = "5TL";
+                }
+                else
+                {
+                    lblUcret.Text = "3.5TL";
+                }
+            }
+            else if (cmbDurum.SelectedIndex == 3)
+            {
+                if (yas <20 && yas>65)
+                {
+                    lblUcret.Text = "5TL";
+                }
+                else
+                {
+                    lblUcret.Text = "2.5TL";
+                }
+            }
+            else if (cmbDurum.SelectedIndex == 4)
+            {
+                if (yas <60)
+                {
+                    lblUcret.Text = "5TL";
+                }
+                else
+                {
+                    lblUcret.Text = "3.75TL";
+                }
+            }
+            else if (cmbDurum.SelectedIndex == 5)
+            {
+                if (yas <60)
+                {
+                    lblUcret.Text = "5TL";
+                }
+                else
+                {
+                    lblUcret.Text = "3.75";
+                }
+            }
+        }
+
+        private void cmbDurum_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtAdSoyad_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
